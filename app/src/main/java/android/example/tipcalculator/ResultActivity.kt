05b1ef66bacabb481540ee9f6fb5280dc47710e2
Abhi Khandelwal <khandelwalabhi386@gmail.com>
2021-12-19 11:28:12 +0530

    without data binding

--- app/src/main/java/android/example/tipcalculator/ResultActivity.kt
+++ app/src/main/java/android/example/tipcalculator/ResultActivity.kt
@@ -1,21 +1,16 @@
 package android.example.tipcalculator
 
-import android.example.tipcalculator.databinding.ActivityMainBinding
-import android.example.tipcalculator.databinding.ActivityResultBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_result.*
 import java.text.NumberFormat
 
 class ResultActivity : AppCompatActivity() {
-    lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val tip= intent.getStringExtra("tip")?.toDoubleOrNull()
-        binding=ActivityResultBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        val formatedTip= NumberFormat.getCurrencyInstance().format(tip)
-        binding.tipResult.text=getString(R.string.tip_amount,formatedTip)
-
+        setContentView(R.layout.activity_result)
+        val tip=intent.getDoubleExtra("tip", 0.00)
+        val formattedNumber=NumberFormat.getCurrencyInstance().format(tip)
+        tipResult.text=getString(R.string.tip_amount,formattedNumber)
     }
 }
